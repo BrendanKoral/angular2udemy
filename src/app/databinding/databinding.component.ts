02e5b972fd41c5a2b7027b1030a1c0cb1cdb0859
Brendan Koral <brendanhkoral@gmail.com>
2016-08-31 19:08:15 -0500

    Some basic properties binding

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -5,12 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: `
   {{stringInterpolation}}!!!
   {{numberInterpolation}}
+  
+  <h1>Property Binding 1</h1>
+    <input type="text" value="{{stringInterpolation}}">
+    
+    <h1>Property Binding 2</h1>
+    <input type="text" [value]="stringInterpolation">
+    
+    <p [ngClass]="{testMe: onResult()}">Is this styled?</p>
+    <p [ngStyle]="{color: 'blue'}">HOW ABOUT THIS THOUGH???</p>
 `,
   styleUrls: ['databinding.component.css']
 })
 export class DatabindingComponent implements OnInit {
   stringInterpolation = 'This is string interpolation';
   numberInterpolation = 2;
+
+  onResult(){
+    return false;
+  }
 
   constructor() { }
 
